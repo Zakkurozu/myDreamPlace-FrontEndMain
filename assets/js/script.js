c92fa9b5a21e0bec66c4f5589335cda3918479fb
Zakkurozu <afrizalahma5@gmail.com>
2024-09-21 10:35:16 +0700

    slicing-end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function checkScreenWidth() {
-        if (window.innerWidth < 1024) {
-            let menulist = document.getElementById('menu-list');
-            menulist.style.height = '0px'; 
-            menulist.style.padding = '0px'; 
-            let togglebtn = document.getElementById('togglemenu');
-            let menuIsOpen = false;
+  function checkScreenWidth() {
+    if (window.innerWidth < 1024) {
+      let menulist = document.getElementById("menu-list");
+      menulist.style.height = "0px";
+      menulist.style.padding = "0px";
+      let togglebtn = document.getElementById("togglemenu");
+      let menuIsOpen = false;
 
-            function togglemenu(){
-                if(menuIsOpen == false){
-                    menulist.style.padding = '1.2rem 0';
-                    menulist.style.height = '14.5rem';
-                    menuIsOpen = true;
-                }else{
-                    menulist.style.height = '0px';
-                    menulist.style.padding = '0px';
-                    menuIsOpen = false;
-                }
-                // console.log(menuIsOpen);
-            }
-
-            togglebtn.addEventListener('click', togglemenu);
+      function togglemenu() {
+        if (menuIsOpen == false) {
+          menulist.style.padding = "1.2rem 0";
+          menulist.style.height = "14.5rem";
+          menuIsOpen = true;
+        } else {
+          menulist.style.height = "0px";
+          menulist.style.padding = "0px";
+          menuIsOpen = false;
         }
-    }    
-    checkScreenWidth();
-    window.addEventListener('resize', checkScreenWidth);
+        // console.log(menuIsOpen);
+      }
+
+      togglebtn.addEventListener("click", togglemenu);
+    }
+  }
+  checkScreenWidth();
+  window.addEventListener("resize", checkScreenWidth);
+});
+
+// date pick
+flatpickr("#date-pick", {
+  dateFormat: "d-m-y",
+  allowInput: true,
+  disableMobile: true,
 });
